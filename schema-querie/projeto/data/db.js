f21e2ec8f87c23e3e2c7024f0bf106989d947294
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-27 03:37:58 -0300

    Aula 28 - Enum

--- schema-querie/projeto/data/db.js
+++ schema-querie/projeto/data/db.js
@@ -4,11 +4,11 @@ const perfis_list = [
 ]
 
 const users_list = [
-  { id: "1", nome: "Ana", email: "ana@email.com", idade: 23, id_perfil: "1"},
-  { id: "2", nome: "Maria", email: "maria@email.com", idade: 45, id_perfil: "2"},
-  { id: "3", nome: "Pedro", email: "pedro@email.com", idade: 12, id_perfil: "2"},
-  { id: "4", nome: "Lucia", email: "lucia@email.com", idade: 76, id_perfil: "1"},
-  { id: "5", nome: "Marcos", email: "marcos@email.com", idade: 43, id_perfil: "1" }
+  { id: "1", nome: "Ana", email: "ana@email.com", idade: 23, id_perfil: "1", status: "BLOQUEADO"},
+  { id: "2", nome: "Maria", email: "maria@email.com", idade: 45, id_perfil: "2", status: "ATIVO"},
+  { id: "3", nome: "Pedro", email: "pedro@email.com", idade: 12, id_perfil: "2", status: "BLOQUEADO"},
+  { id: "4", nome: "Lucia", email: "lucia@email.com", idade: 76, id_perfil: "1", status: "ATIVO"},
+  { id: "5", nome: "Marcos", email: "marcos@email.com", idade: 43, id_perfil: "1", status: "INATIVO" }
 ];
 
 module.exports = {
