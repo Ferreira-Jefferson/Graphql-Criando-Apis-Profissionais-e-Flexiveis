75e5f4dca92e97e66061ac2877823017633a3d2a
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-27 01:26:44 -0300

    Aula 21 - Passando Parametros para as Consultas

--- schema-querie/projeto/index.js
+++ schema-querie/projeto/index.js
@@ -1,5 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const car_list = [
+  { id: 1, nome: "IX 35", marca: "Hyundai", ano: 2019, preco: 89900.01 },
+  { id: 2, nome: "Sportage", marca: "Kia", ano: 2015, preco: 64900.01 },
+  { id: 3, nome: "XC40", marca: "Volvo", ano: 2018, preco: 169950.01 }
+];
+
 const typeDefs = gql`
   type Carro {
     id: ID
@@ -12,16 +18,15 @@ const typeDefs = gql`
   type Query {
     numerosMegaSena: [Int!]
     carros: [Carro!]!
+    carro(id: ID): Carro
   }
 `
 
 const resolvers = {
   Query: {
     numerosMegaSena: () => new Array(6).fill().map(() => parseInt(Math.random() * 60 + 1)),
-    carros: () => [
-      { id: 1, nome: "IX 35", marca: "Hyundai", ano: 2019, preco: 89900.01},
-      { id: 2, nome: "Sportage", marca: "Kia", ano: 2015, preco: 64900.01}
-    ]
+    carros: () => car_list,
+    carro: (_, { id }) => car_list.filter(car => car.id == id)[0]
   }
 }
 
