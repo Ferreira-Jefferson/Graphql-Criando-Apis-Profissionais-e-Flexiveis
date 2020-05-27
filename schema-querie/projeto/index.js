e3e5bd4c6db4646c529b317698d619ec18511155
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-26 21:22:42 -0300

    Aula 10 - Primeira Query

--- schema-querie/projeto/index.js
+++ schema-querie/projeto/index.js
@@ -1,8 +1,20 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const typeDefs = gql``
+const typeDefs = gql`
+  #Pontos de entrada da API
+  type Query {
+    ola: String
+    idade: Int
+  }
+`
 
-const resolvers = {}
+
+const resolvers = {
+  Query: {
+    ola: () => "Hello World!",
+    idade: () => Math.floor(Math.random() * 30)
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
