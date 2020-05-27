412d8939287c002bece096e9fd0558b514133531
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-27 00:09:10 -0300

    Aula 16 e 17 - Desafio Produto

--- schema-querie/projeto/index.js
+++ schema-querie/projeto/index.js
@@ -12,14 +12,28 @@ const typeDefs = gql`
     vip: Boolean
   }
 
+  type Produto {
+    id: ID
+    nome: String!
+    preco: Float!
+    desconto: Int
+    precoComDesconto: Float
+  }
+
   type Query {
     horaAtual: Date!
     usuarioLogado: Usuario
+    produtoEmDestaque: [Produto]
   }
 `
 const resolvers = {
   Usuario: {
     salario: usuario => usuario.salario_real //Resolvendo campo com nome diferente
+  },
+  Produto: {
+    precoComDesconto: produto => produto.desconto ?
+      (produto.preco * (1 - produto.desconto / 100)).toFixed(2)
+      : produto.preco
   },
   Query: {
     horaAtual: () => new Date(),
@@ -30,8 +44,19 @@ const resolvers = {
       idade: 25,
       salario_real: 123.45, //Simulando: Campo com nome diferente no banco
       vip: true
-    })
-  }
+    }),
+    produtoEmDestaque: () => [{
+        id: 1,
+        nome: "TV 29'",
+        preco: 1002.34,
+        desconto: 10
+      },
+      {
+        id: 1,
+        nome: "Notebook'",
+        preco: 2354.76
+      }]
+  } 
 }
 
 const server = new ApolloServer({
