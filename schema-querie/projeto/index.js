59a2aac59d248fd487bc7d297a474370e3b097a2
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-27 02:09:37 -0300

    Aula 24 - Usando Resolver para Relacionar Tipos

--- schema-querie/projeto/index.js
+++ schema-querie/projeto/index.js
@@ -1,26 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const perfis_list = [{ id: "1", nome: "Comum" }, { id: "2", nome: "Administrador" }]
+
 const users_list = [
-  { id: "1", nome: "Ana", email: "ana@email.com", idade: 23 },
-  { id: "2", nome: "Maria", email: "maria@email.com", idade: 45 },
-  { id: "3", nome: "Pedro", email: "pedro@email.com", idade: 12 },
-  { id: "4", nome: "Lucia", email: "lucia@email.com", idade: 76 },
-  { id: "5", nome: "Marcos", email: "marcos@email.com", idade: 43 }
+  { id: "1", nome: "Ana", email: "ana@email.com", idade: 23, id_perfil: "1"},
+  { id: "2", nome: "Maria", email: "maria@email.com", idade: 45, id_perfil: "2"},
+  { id: "3", nome: "Pedro", email: "pedro@email.com", idade: 12, id_perfil: "2"},
+  { id: "4", nome: "Lucia", email: "lucia@email.com", idade: 76, id_perfil: "1"},
+  { id: "5", nome: "Marcos", email: "marcos@email.com", idade: 43, id_perfil: "1" }
 ];
-const perfis_list = [{id: "1", nome: "Comum"}, {id: "2", nome: "Administrador"}]
 
 const typeDefs = gql`
+  type Perfil {
+    id: ID!
+    nome: String!
+  }
+
   type Usuario {
     id:ID
     nome:String!
     email:String!
     idade: Int!
+    perfil: Perfil
   }
 
-  type Perfil {
-    id: ID!
-    nome: String!
-  }
+
 
   type Query {
     usuario(id: ID): Usuario
@@ -32,6 +36,10 @@ const typeDefs = gql`
 `
 
 const resolvers = {
+  Usuario: {
+    perfil: ({id}) => perfis_list.filter(perfil => perfil.id === id)[0]
+  },
+
   Query: {
     usuario: (_, { id }) => users_list.filter(user => user.id === id)[0],
     usuarios: () => users_list,
