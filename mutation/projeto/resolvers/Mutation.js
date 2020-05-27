78c0c29d02959e95fde0663ef54d4284108766c4
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-27 11:29:27 -0300

    Aula 34 - Validar E-mail para Evitar Usuarios Duplicados

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -2,7 +2,9 @@ const { usuarios, IDGenerator } = require('../data/db')
 
 module.exports = {
   novoUsuario(_, { nome, email, idade }) {
-    console.log(nome, email, idade)
+    if (usuarios.some(user => user.email === email))
+      throw new Error("Usuário já cadastrados!")
+    
     const novoUsuario = {
       id: IDGenerator(),
       nome,
