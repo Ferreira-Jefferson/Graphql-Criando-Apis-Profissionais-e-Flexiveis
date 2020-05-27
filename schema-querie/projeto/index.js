79a0487637f98403153cd0dd0ff02abc56ba481f
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-05-26 22:59:04 -0300

    Aula 14 - Criando Primeiro Tipo

--- schema-querie/projeto/index.js
+++ schema-querie/projeto/index.js
@@ -3,14 +3,31 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
   scalar Date
 
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   type Query {
-    horaAtual: Date
+    horaAtual: Date!
+    usuarioLogado: Usuario
   }
 `
-
 const resolvers = {
   Query: {
-    horaAtual: () => new Date()
+    horaAtual: () => new Date(),
+    usuarioLogado: () => ({
+      id: 1,
+      nome: "Jefferson",
+      email: "jefferson@email.com",
+      idade: 25,
+      salario: 123.45,
+      vip: true
+    })
   }
 }
 
